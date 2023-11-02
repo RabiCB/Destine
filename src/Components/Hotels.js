8cf40ada35915809ebf325e95baa85382be19634
Chandra Bhandari <rollinrabin@gmail.com>
2023-11-02 08:51:08 +0545

    feat:somethingh

--- src/Components/Hotels.js
+++ src/Components/Hotels.js
@@ -34,7 +34,7 @@ const Hotels =() => {
   }
 
   const {data:sss,isLoading:ss}=useQuery({queryKey:['getSearch'],queryFn:getSearch})
-console.log(sss)
+
   
 
 
@@ -54,11 +54,11 @@ console.log(sss)
    }
   return (
   <div className='relative top-[72px] bottom-0 right-0  left-0'>
-    <div className='grid grid-cols-4   p-6 max-lg:grid-cols-3 gap-4 max-md:grid-cols-3 max-sm:grid-cols-2'>
+    <div className='grid grid-cols-4   p-6 max-lg:grid-cols-3 gap-4 max-md:grid-cols-3 max-sm:grid-cols-1'>
     {
       data?.map((hotels)=>{
         return<Link to={"/information/accomodation/"+ hotels?._id}><div className=' flex items-start justify-start flex-col gap-[4px]'>
-          <img className='rounded-2xl object-cover aspect-square' src={"https://airbnbclone-3off.onrender.com/uploads/"+ hotels.photos[0]}/>
+          <img alt="hotelimg" className='rounded-2xl object-cover aspect-square ' src={"https://airbnbclone-3off.onrender.com/uploads/"+ hotels.photos[0]}/>
           <span className="font-bold ">{hotels?.address}</span>
           <p className='text-[14px] text-slate-500'>{hotels?.description.substring(0,24)}...</p>
           <p className='text-[14px] text-slate-500'>{hotels?.extraInfo}</p>
